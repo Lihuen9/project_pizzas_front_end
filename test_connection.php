c32f9f0b8e85caab1b65e98406e2b50cd0f31aab
Lihuen9 <104942924+Lihuen9@users.noreply.github.com>
2022-08-03 19:07:13 -0300

    Add files via upload

--- test_connection.php
+++ test_connection.php
@@ -1,11 +1,25 @@
 <?php
 include_once "config_login.php";
 
+
+function openCom()
+{
 $dsn = "mysql:dbname=".DATABASE_NAME.";host=".SERVER_NAME;
-
 $link = new PDO ($dsn, USER_NAME, PASSWORD);
+// echo "conexion exitosa";
+return $link;
+}
 
-echo "conexion exitosa"
+function closeCon($a){
+    $a=null;
+}
 
+// Invocación 
+$con=openCom();
+
+// Operaciones CRUD
+echo "CRUD";
+
+closeCon($con);
 
 ?>
