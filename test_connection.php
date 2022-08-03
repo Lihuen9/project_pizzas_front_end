0b2e0f6a2b7019284286aa5bd485682e795b6f99
Lihuen9 <104942924+Lihuen9@users.noreply.github.com>
2022-08-03 20:33:48 -0300

    Add files via upload

--- test_connection.php
+++ test_connection.php
@@ -18,7 +18,22 @@ function closeCon($a){
 $con=openCom();
 
 // Operaciones CRUD
-echo "CRUD";
+// echo "CRUD";
+$sql="select username, email from users";
+$row=$con->query($sql); //conexion a la base de datos, consulta y devuelve un DATASET (conjunto de datos).
+echo "<h1> Listado </h1>";
+foreach($row as $col) { //recorre un array asociativo
+    echo "<br>";
+    echo $col ["username"];
+    echo "<br>";
+    echo $col ["email"];
+    echo "<br>";
+    echo "---------------";
+    
+
+
+}
+
 
 closeCon($con);
 
