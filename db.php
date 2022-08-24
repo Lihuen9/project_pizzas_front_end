d05363d2c5b5229a3435672ec61b9a9b1966a85b
Lihuen9 <104942924+Lihuen9@users.noreply.github.com>
2022-08-24 20:29:21 -0300

    Add files via upload

--- db.php
+++ db.php
@@ -1,6 +1,32 @@
 <?php
+
+/**
+ * -----------------------------
+ * Clase para manejar base de doatos, usando PDO.
+ * 
+ * @author Creado por mí, genio! <capo@capo.com>
+ * @link https://github.com/Lihuen9/project_pizzas_front_end
+ * @version 1.0.0.0.9
+ * @copyright 2022 Curso Programación Web
+ *  
+ * -----------------------------
+ */
+
+
+ 
+
+
+
+
 class DB {
     private $connection;
+
+    /**
+     * Establecer una conexión.
+     * @return connection
+     */
+
+
     public function __construct()
     {
         try {
@@ -16,7 +42,47 @@ class DB {
         return $this->connection;
     } 
 
+
+
+   
+
+    /**
+     * Prepara una query SQL.
+     *
+     * @return object
+     */
+
+    public function prepare($query)
+    {
+        return $this->connection->prepare($query);
+    }
+
+
+    /**
+     * Ejecuta una sql query
+     *
+     * @param string $query
+     * @return object
+     */
+
+    public function query($query)
+    {
+        return $this->connection->query($query);
+    }
     
+    /**
+     * Cierra una conexión
+     *
+     * @return mixed
+     */
+
+
+    public function close()
+    {
+        $this->connection=null;
+    }
+
 }
+
 
 ?>
