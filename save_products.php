6a8c575c81bba6f351a56d4e88473307e4d15960
Lihuen9 <104942924+Lihuen9@users.noreply.github.com>
2022-09-28 20:23:18 -0300

    Add files via upload

--- save_products.php
+++ save_products.php
@@ -2,6 +2,57 @@
 $products=$_POST['producto'];
 $price=$_POST['precio'];
 $category=$_POST['categoria'];
-echo $products . $price . $category;
+
+/* para verificaciòn:
+   echo $products . $price . $category; */
+
+
+include_once("config_products.php");
+include_once("db.php");
+
+$link=new Db();
+include_once("upload_image.php");
+$path_img=$directorio.$nombre_img; //Ruta de la imagen levantada
+
+$sql="INSERT into products (product_name, image, price, id_category) VALUES (:product,:path_img,:price,:category)";
+
+$stmt = $link->prepare($sql);
+
+$stmt ->bindValue('product', $products);
+$stmt ->bindValue('price', $price);
+$stmt ->bindValue('category', $category);
+$stmt ->bindValue('path_img', $path_img); //No es necesario sanear
+
+if ($stmt->execute()) {
+    ?>
+    <script>
+        alert("Producto ingresado!");
+        window.location="insert_products.php";
+        </script>
+    <?php
+      
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 ?>
